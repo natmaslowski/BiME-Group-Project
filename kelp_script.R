a2976f9f1eb15efff8e56480ad672da97bc781bd
natmaslowski <56177958+natmaslowski@users.noreply.github.com>
2020-07-06 16:12:56 -0700

    Delete kelp_script.R

--- kelp_script.R
+++
@@ -1,3 +0,0 @@
-#This is a test to see if people have access to this script. If you do. Please edit something here. Save it, then "push it in my direction" so I can see if this is working.
-#let's see if this works lol
-#I TTHINK WE DID IT!
