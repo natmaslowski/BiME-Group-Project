87f1d60bb0a8e5a363517bc3e0f787428afe2a27
Natalie Maslowski <natmaslowski@gmail.com>
2020-07-06 18:11:06 -0700

    updatessss

--- kelp_turf_script.R
+++ kelp_turf_script.R
@@ -3,30 +3,30 @@
 #Date of creation: July 6, 2020
 
 
-# Loading of packages -----------------------------------------------------
+# Loading of packages ----------------------------------------------------------------------
 #Space for loading packages. (Jess which ones do you think we will need? dplyr, stringr, ggplot2?)
 
 
-# Data Wrangling ----------------------------------------------------------
+# Data Wrangling ---------------------------------------------------------------------------
 # read.csv <-
 
 
-# Figure 1. Site Map ------------------------------------------------------
+# Figure 1. Site Map -----------------------------------------------------------------------
 
 
 
-# Figure 2. Abundance of kelp/turf cover as a function of wave exposure. ---------------------------------------------------------------
+# Figure 2. Abundance of kelp/turf cover as a function of wave exposure. -------------------
 
 
-# Figure 3. Abundance of kelp/turf cover as a function of temperature.---------------------------------------------------------------
+# Figure 3. Abundance of kelp/turf cover as a function of temperature.----------------------
 
 
-# Figure 4. Species richness of kelp/turf cover as a function of wave exposure.---------------------------------------------------------------
+# Figure 4. Species richness of kelp/turf cover as a function of wave exposure.-------------
 
 
-# Figure 5. Species richness of kelp/turf cover as a function of temperature.---------------------------------------------------------------
+# Figure 5. Species richness of kelp/turf cover as a function of temperature.---------------
 
 
-# Generalized liner effects model for the interaction between temp --------
+# Generalized liner effects model for the interaction between temp -------------------------
 
 
