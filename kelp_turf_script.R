d0a0c7e4684962d50641246ed1dcc30ec15779ec
Natalie Maslowski <natmaslowski@gmail.com>
2020-07-06 17:17:48 -0700

    updates by nat

--- kelp_turf_script.R
+++ kelp_turf_script.R
@@ -1,3 +1,7 @@
 #BiME Kelp vs Turf Working Script. 
 #Authors: Natalie M. and Jess K.
 #Date of creation: July 6, 2020
+
+#Space for loading packages. (Jess which ones do you think we will need? dplyr, stringr, ggplot2?)
+
+# read.csv <-
