adda69f15c5d2deda754c1b9f6d564e355866125
Natalie Maslowski <natmaslowski@gmail.com>
2020-07-10 14:02:28 -0700

    updates

--- kelp_turf_script.R
+++ kelp_turf_script.R
@@ -6,20 +6,24 @@
 # Loading of packages ----------------------------------------------------------------------
 #Space for loading packages. (Jess which ones do you think we will need? dplyr, stringr, ggplot2?)
 
+library(ggplot2)
+library(stringr)
+library(tidyr)
+library(dplyr)
 
 # Data Wrangling ---------------------------------------------------------------------------
-# read.csv <-
+kelp_data <- read.csv("kelp_turf_data.csv")
 
 
 # Figure 1. Site Map -----------------------------------------------------------------------
 
-
+#See Nik's image 
 
 # Figure 2. Abundance of kelp/turf cover as a function of wave exposure. -------------------
-
+# started on this, but realized we don't have wave exposure data yet. ggplot(kelp_data, aes(x=)
 
 # Figure 3. Abundance of kelp/turf cover as a function of temperature.----------------------
-
+ggplot
 
 # Figure 4. Species richness of kelp/turf cover as a function of wave exposure.-------------
 
